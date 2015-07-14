eeba74b4944e7782f51988e9299168b9f66dabdd
David Holland <david.holland@defensesoftware.com>
2015-07-14 14:35:58 -0400

    Initial commit 7/14/2015

---
+++ README.txt
@@ -0,0 +1,16 @@
+Example of Cross-Site Web Service using WCF, JSONp, Ajax, CSS.
+
+To Execute:
+
+1.  Launch IDE.
+2.  Set MenuWcfRestService as Startup project
+3.  Run w/o debugging.
+4.  Return to IDE.
+5.  Set RestaurantSite as Startup project and order_dev.htm as startup file.
+6.  Create C:\RestaurantOrders folder.
+7.  Run w/o debugging.
+8.  Place your orders!
+
+
+
+
---
+++ RestaurantDemoSite/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RestaurantDemoSite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestaurantDemoSite")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8d40dd1-fb0c-4b7b-9cd2-4133c17c4bea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
